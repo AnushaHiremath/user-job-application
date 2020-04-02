4779ba484f31493b0e15825a16bf11f7a431908a
AnushaHiremath <hiremath.anusha@gmail.com>
2020-04-02 11:34:40 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import User from './User'
+import Dashboard from './Dashboard'
+function  App(props) {
+    return( 
+        <div>
+            <User/>
+            <Dashboard/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class UserList extends React.Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Admin Dashboard</h2>
+                
+            </div>
+        )
+    }
+}
+
+
+export default UserList
